b4212e7110889a2ba3d281bc40c0c27157a84970
Lee <inbox@leefallat.ca>
2021-11-13 04:37:37 -0500

    Initial working commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Curv Playground
+
+Write Curv code in your browser! The playground will send your code to the server
+where its processed by Curv and returns a model to be viewed.
+
+In the future Curv will return shader code which will run in a WebGPU context.
+
+DO NOT USE THIS WHEN ON MOBILE DATA! Each render is about 2MB and will eat
+your bandwidth.
+
+Visit https://playground.curv3d.org !
+
+## Setup your own
+
+Make sure nodejs, npm and Curv is installed on the server. That should be all!
+
+curv-playground will listen on port 9000 on localhost. This can be easily changed
+in server.js.
+
+## Context
+
+I did this in the course of 7 hours, staying up from 10pm to 5am. It is not 
+pretty but gets the job done reasonably.
---
+++ server.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { spawn } = require('child_process');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('.'));
+app.use(express.text());
+
+app.post('/render-3d', (req, res) => {
+  const code = req.body;
+  
+  const child = spawn(
+    'curv',
+    ['-o', 'gltf', '-O', 'jit', '-O', 'vsize=0.6', '-']
+  );
+  child.stdin.write(code);
+  child.stdin.end();
+
+  
+  let statusSet = false;
+  let contentTypeSet = false;
+  let isSuccess = false;
+  let skipLines = 0;
+
+  child.stdout.on('data', (data) => {
+    res.write(data);
+  });
+  
+  child.stderr.on('data', (data) => {
+    if (data.indexOf('ERROR') < 0) { return; }
+    if (!statusSet) { res.status(500);  statusSet = true; }
+    if (!contentTypeSet) { res.append('Content-Type', 'plain/text'); contentTypeSet = true; }
+
+    res.write(data);
+  });
+
+  child.on('close', (code) => {
+    res.end();
+  });
+});
+
+app.listen(9000, () => console.log("Listening"));
