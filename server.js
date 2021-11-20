805e1507f138cbc356ab826d655d1a41c6ea5660
lf94 <inbox@leefallat.ca>
2021-11-19 21:25:28 -0500

    Switch to an image-based solution

--- server.js
+++ server.js
@@ -7,34 +7,64 @@ const app = express();
 app.use(express.static('.'));
 app.use(express.text());
 
-app.post('/render-3d', (req, res) => {
-  const code = req.body + " >> extrude 0.00000001 >> set_bbox [640, 640, 640]";
+function* getXDisplayNumber() {
+    const startValue = 99;
+    let i = startValue;
+    while(true) {
+        i -= 1;
+
+        // Never hit zero since 0 is usually used by desktop users.
+        if (i <= 0) i = startValue;
+        yield ':' + i;
+    }
+}
+
+app.post('/render', (req, res) => {
+  const code = req.body;
+  const DISPLAY = getXDisplayNumber().next().value;
   
-  const child = spawn(
-    'curv',
-    ['-o', 'gltf', '-O', 'jit', '-O', 'vsize=0.6', '-']
+  const xvfbProcess = spawn(
+     'Xvfb',
+     [DISPLAY, '-ac', '-nocursor', '-screen', '0', '480x500x24'],
   );
-  child.stdin.write(code);
-  child.stdin.end();
+
+  const curvProcess = spawn(
+     'curv',
+     ['-' ],
+    { env: Object.assign({}, process.env, { DISPLAY, PATH: '/usr/local/bin' }) }
+  );
+
+  curvProcess.stdin.write(code);
+  curvProcess.stdin.end();
 
   let statusSet = false;
   let contentTypeSet = false;
-  let isSuccess = false;
-  let skipLines = 0;
 
-  child.stdout.on('data', (data) => {
-    res.write(data);
-  });
-  
-  child.stderr.on('data', (data) => {
+  curvProcess.stderr.on('data', (buf) => {
+    const data = buf.toString('utf8');
+    if (data.indexOf('shape') >= 0) {
+        if (!statusSet) { res.status(200);  statusSet = true; }
+        if (!contentTypeSet) { res.append('Content-Type', 'image/png'); contentTypeSet = true; }
+
+      setTimeout(() => {
+        const screenshotProcess = spawn('maim', ['--hidecursor'], { env: { DISPLAY } });
+        screenshotProcess.stdout.on('data', (data) => { res.write(data); });
+        screenshotProcess.on('close', () => {
+          res.end();
+          curvProcess.kill();
+          xvfbProcess.kill();
+        });
+      }, 1000);
+    }
+
     if (data.indexOf('ERROR') < 0) { return; }
+    curvProcess.kill();
+    xvfbProcess.kill();
+
     if (!statusSet) { res.status(500);  statusSet = true; }
     if (!contentTypeSet) { res.append('Content-Type', 'plain/text'); contentTypeSet = true; }
 
     res.write(data);
-  });
-
-  child.on('close', (code) => {
     res.end();
   });
 });
